6ac5ed26e4437e15fbec61964627b062f1b92d0f
PO CHANG CHEN <kk741852963tw1610@gmail.com>
2022-07-14 15:36:29 +0800

    better way 3

--- leetcode/medium/3. Longest Substring Without Repeating Characters.js
+++ leetcode/medium/3. Longest Substring Without Repeating Characters.js
@@ -3,30 +3,38 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  var temp = '';
+  // var temp = '';
+  // var max = 0;
+  // for (var i = 0; i < s.length; i++) {
+  //     if (!temp.includes(s[i])) {
+  //         if (i === s.length - 1) {
+  //             temp += s[i];
+  //              if (temp.length > max) {
+  //                 max = temp.length;
+  //             }
+  //         } else {
+  //             temp += s[i];
+  //         }
+  //     } else {
+  //         if (temp.length > max) {
+  //             max = temp.length;
+  //         }
+  //         var index = temp.indexOf(s[i]);
+  //         temp = temp.substring(index + 1);
+  //         temp += s[i];
+  //     }
+  // }
+  // return max;
+  var temp = new Set();
   var max = 0;
-  for (var i = 0; i < s.length; i++) {
-    if (!temp.includes(s[i])) {
-      if (i === s.length - 1) {
-        temp += s[i];
-          if (temp.length > max) {
-            max = temp.length;
-        }
-      } else {
-        temp += s[i];
+  var l = 0;
+  for (let i = 0; i < s.length; i++) {
+      while (temp.has(s[i])) {
+          temp.delete(s[l]);
+          l++;
       }
-    } else {
-      if (temp.length > max) {
-        max = temp.length;
-        var index = temp.indexOf(s[i]);
-        temp = temp.substring(index + 1);
-        temp += s[i];
-      } else {
-        var index = temp.indexOf(s[i]);
-        temp = temp.substring(index + 1);
-        temp += s[i];
-      }
-    }
+      temp.add(s[i]);
+      max = Math.max(max, i - l + 1);
   }
   return max;
 };
